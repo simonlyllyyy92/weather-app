788cd6e4733080a14ac4613386c9b74138c85596
Simon <simon@DaviddeiMac.local>
2021-11-11 22:59:51 +1100

    change base url

--- src/axios/http_client.ts
+++ src/axios/http_client.ts
@@ -4,7 +4,7 @@ import axios from "axios";
  * We set up the baseUrl for all api requests
  */
 
-export const baseUrl = "http://api.weatherapi.com/v1";
+export const baseUrl = "https://api.weatherapi.com/v1";
 
 // Can only receive the response for the upcoming TWO days data from weather.api
 export const httpClient = axios.create({
